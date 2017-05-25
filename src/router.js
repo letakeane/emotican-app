5e1d8fe0c0130b09150643a4abb5b8a65f2d586d
Leta Keane <letakeane+turing@gmail.com>
2017-05-25 11:07:06 -0600

    Add helper function to prep API response data for display

--- src/router.js
+++ src/router.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var controller = require('./controller');
-
-  router.post('/emotions', controller.getEmotionAnalysis)
-  router.post('/API/imageURL', controller.postImageURL)
-  router.get('/API/imageURL/:url', controller.getImageURL)
+  // 
+  // router.post('/emotions', controller.getEmotionAnalysis)
+  // router.post('/API/imageURL', controller.postImageURL)
+  // router.get('/API/imageURL/:url', controller.getImageURL)
 
 module.exports = router;
