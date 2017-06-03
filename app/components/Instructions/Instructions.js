e975a08415a06d3fd1892b2fc2ea78cb7f41a01c
Leta Keane <letakeane+turing@gmail.com>
2017-06-02 22:23:27 -0600

    Write instructions

--- app/components/Instructions/Instructions.js
+++ app/components/Instructions/Instructions.js
@@ -2,6 +2,20 @@ import React from 'react';
 
 export const Instructions = () => {
   return (
-    <div>Instructions go here</div>
+    <article className='instructions'>
+      <h2 className='instructions-howto'>How to play</h2>
+      <img  className='instructions-img'
+            src='../../assets/images/instructions.jpg'
+            alt='screenshot of emotican app' />
+      <ol className='instruction-list'>
+        <li>Pick an emotion to express by clicking one of the yellow buttons</li>
+        <li>Click the "Start video" button</li>
+        <li>Practice showing the emotion!</li>
+        <li>Click the "take picture" button</li>
+        <li>Learn more about what emotions your expression shows!</li>
+        <li>Try again or pick a new emotion to try</li>
+        <li>Have fun!</li>
+      </ol>
+    </article>
   )
 }
