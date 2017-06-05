07b76667777456e4eb6cc2a42c2f075075ca621b
Leta Keane <letakeane+turing@gmail.com>
2017-06-05 15:03:59 -0600

    Change lets to consts

--- app/lib/analyzeResponse.js
+++ app/lib/analyzeResponse.js
@@ -1,10 +1,10 @@
 const analyzeResponse = (data) => {
-  let scores = data[0].scores;
-  let emotions = Object.keys(scores);
-  let sortedEmotions = emotions.sort((a, b) => {
+  const scores = data[0].scores;
+  const emotions = Object.keys(scores);
+  const sortedEmotions = emotions.sort((a, b) => {
     return scores[b] - scores[a];
   })
-  let emotionDisplay = sortedEmotions.map(emotion => {
+  const emotionDisplay = sortedEmotions.map(emotion => {
     if (scores[emotion] > 0.49) {
       return {quantifier: 'mostly',
               emotion: emotion.toLowerCase()}
